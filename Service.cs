cfcb6629f7be8ca281a1d753c1e798c0fe066085
Gemeng Vang <gemengvang96@gmail.com>
2022-03-15 14:48:47 -0500

    Completed Service Class

---
+++ Service.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGameDemo
+{
+    public class Service
+    {
+        // Handles business logic for application
+
+        private Random ranNum = new Random();
+        private int _maxNumer;
+
+        public int GuessCount { get; set; }
+        public int TargetNum { get; private set; }
+        public int MaxNum { get; private set; }
+        
+
+        //Used to replace the get/set method within constructors above.
+      /*      public Service(int maxNum)
+        {
+            _maxNumer = maxNum;
+        }
+       */
+
+        public void CreateRandomNumber(int maxValue)
+        {
+            MaxNum = maxValue;
+            TargetNum = ranNum.Next(1, MaxNum + 1);
+        }
+
+
+      
+    }
+}
--- StartUp.cs
+++ StartUp.cs
@@ -10,17 +10,19 @@ namespace GuessingGameDemo
     {
         // TODO: What properties do I need
 
-
+        private Service _service = new Service();
         // Add Methods
         public void Run()
         {
             Console.Write("Running!");
+            int maxNum = ConsoleIO.GetMaxNum();
+            _service.CreateRandomNumber(maxNum);
         }
 
         public void CheckGuess()
         {
             // TODO: Guess a number between one and some maximum.
-
+            ConsoleIO.DisplayMessage($"Guess a number between 1 and ....");
         }
 
     }
